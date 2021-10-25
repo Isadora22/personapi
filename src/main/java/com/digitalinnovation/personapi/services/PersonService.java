00443f46f1a1eb6aa240a1a543a2f261ce5148d8
Isadora22 <isadoralemes19@live.com>
2021-10-25 11:55:25 -0300

    Created README.md file

--- src/main/java/com/digitalinnovation/personapi/services/PersonService.java
+++ src/main/java/com/digitalinnovation/personapi/services/PersonService.java
@@ -1,6 +1,7 @@
 package com.digitalinnovation.personapi.services;
 
 import com.digitalinnovation.personapi.exceptions.PersonNotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,17 +15,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 		
 	private PersonRepository personRepository;
 	
 	private final PersonMapper personMapper = PersonMapper.INSTANCE;
-	
-	@Autowired
-	public PersonService(PersonRepository personRepository) {
-		this.personRepository = personRepository;
-	}
-	
+
 	public MessageResponseDTO createPerson(PersonDTO personDTO) {
 		Person personToSave = personMapper.toModel(personDTO);
 		Person savedPerson = personRepository.save(personToSave);
